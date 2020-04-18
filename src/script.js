423c28f3f1e6babd1cfeb79e9de525bdd2af5a8e
Haley Sulcer <hjsulcer@gmail.com>
2020-04-18 16:56:56 -0400

    Updated bio and change dark-mode evt handler to jQuery

--- src/script.js
+++ src/script.js
@@ -1,18 +1,17 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+  let acc = document.getElementsByClassName("accordion");
+  let i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 
   $(".sec-about, .sec-exp, .sec-contact").click(function(e) {
     if
@@ -34,14 +33,10 @@ for (i = 0; i < acc.length; i++) {
       return;
   })
 
-
-
-
-
-
-  document.getElementById('dark-mode-btn').addEventListener('click', function (e) {
+  $("#dark-mode-btn").click(function(e) {
     const toggler = document.body;
     toggler.classList.toggle('dark-mode');
+
     const target = e.target;
     target.classList.toggle('fa-moon');
     target.classList.toggle('fa-sun');
